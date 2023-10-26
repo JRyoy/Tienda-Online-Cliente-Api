999e48473f4d057ef284630edce2c80cebea34c1
josbert <josbert708@gmail.com>
2023-10-26 10:16:47 -0300

    Categoria

--- TiendaOnline/Varios/Categoria.cs
+++ TiendaOnline/Varios/Categoria.cs
@@ -3,12 +3,15 @@ namespace Varios;
 public class Categoria
 {
     public Guid IdCategoria { get; protected set; } = Guid.NewGuid();
-    public string Nombre {get; set;}
-    public string Descripcion{get; set;}
-    public Categoria(Guid Id,string Nombre, string Descripcion)
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    List<Producto> Productos { get; set; }
+
+    public Categoria(Guid Id, string Nombre, string Descripcion)
     {
-        this.IdCategoria=IdCategoria;
-        this.Nombre=Nombre;
-        this.Descripcion=Descripcion;
+        this.IdCategoria = IdCategoria;
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.Productos = new List<Producto>();
     }
 }
--- TiendaOnline/Varios/Producto.cs
+++ TiendaOnline/Varios/Producto.cs
@@ -2,24 +2,24 @@ namespace Varios;
 
 public class Producto
 {
-    public Guid Id { get; protected set; } 
-    public string Nombre {get; set;}
-    public double Precio {get;set;}
-    public string Descripcion{get; set;}
-   public string categoria{get; set;}
-    public int CantidadProducto{get; set;}
-    
-    public Producto(Guid Id,string Nombre,double Precio,string Descripcion,string categoria)
+    public Guid Id { get; protected set; }
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+    public string Descripcion { get; set; }
+    public Categoria categoria { get; set; }
+    public int CantidadProducto { get; set; }
+
+    public Producto(Guid Id, string Nombre, double Precio, string Descripcion, Categoria categoria)
     {
-        
-        this.Id=Id;
-        Validacion.ValidacionValor(Precio,"error precio");
-        this.Precio=Precio;
-        Validacion.ValidacionCadena(Nombre,"error Nombre");
-        this.Nombre= Nombre;
-        Validacion.ValidacionCadena(Descripcion,"error descripcion");
-        this.Descripcion=Descripcion;
-        this.categoria=categoria;
+
+        this.Id = Id;
+        Validacion.ValidacionValor(Precio, "error precio");
+        this.Precio = Precio;
+        Validacion.ValidacionCadena(Nombre, "error Nombre");
+        this.Nombre = Nombre;
+        Validacion.ValidacionCadena(Descripcion, "error descripcion");
+        this.Descripcion = Descripcion;
+        this.categoria = categoria;
 
 
     }
